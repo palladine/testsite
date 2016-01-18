49ad8196344ad147c0e7c0054a43a9615f8e8e47
palladine <duroff.83@yandex.ru>
2016-01-18 15:29:26 +0000

    v0.02

--- blogex/settings.py
+++ blogex/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blogex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru_RU'
 
 TIME_ZONE = 'UTC'
 
