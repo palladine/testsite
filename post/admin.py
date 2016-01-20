23257a8f89827b5be8646abad148f2d4b386da26
palladine <duroff.83@yandex.ru>
2016-01-20 15:21:18 +0000

    v0.05

--- post/admin.py
+++ post/admin.py
@@ -3,6 +3,12 @@ from post.models import Tag, Post, Comment
 
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag',)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('post_title',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
 
--- post/models.py
+++ post/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
 class Tag(models.Model):
     class Meta():
         verbose_name = 'Теги'
         verbose_name_plural = 'Теги'
-    tag = models.CharField(max_length = 15, verbose_name='Тег')
+    tag = models.CharField(max_length = 15, verbose_name='Тег', unique=True)
+
 
 class Post(models.Model):
     class Meta():
@@ -16,13 +16,16 @@ class Post(models.Model):
     post_text = models.TextField(verbose_name='Текст поста')
     post_date = models.DateTimeField(verbose_name='Дата и время написания поста', auto_now_add=True)
     post_author = models.CharField(max_length = 25, verbose_name='Автор поста')
-    post_tags = models.ManyToManyField(Tag, verbose_name='Теги поста')
+    post_tags = models.ManyToManyField(Tag, verbose_name='Теги')
+
+
 
 class Comment(models.Model):
     comment_text = models.TextField(verbose_name='Текст комментария')
-    comment_post = models.ForeignKey(Post)
+    comment_post = models.ForeignKey(Post, verbose_name='Комментарии')
     comment_date = models.DateTimeField(verbose_name='Дата и время написания комментария', auto_now_add=True)
     comment_author = models.CharField(max_length = 25, verbose_name='Автор комментария')
+
 
 
 
